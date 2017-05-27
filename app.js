2ec5116c8f1644255532776719ffd96cd54b793b
Sjtsharma <sujit.anand824@gmail.com>
2017-05-28 00:15:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('');
+var exphbs= require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:LoginApp');
+var db = mongoose.connection;
